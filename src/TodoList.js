e8ebd12981769363e1e068f5a7307ef0c98caa28
digitbase <digitbase@gmail.com>
2020-11-22 22:21:56 +0800

    redux 01

---
+++ src/TodoList.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { Input, Button, List } from "antd";
+import store from "./store";
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        console.log(store.getState());
+        this.state = store.getState();
+    }
+    render() {
+        return (
+            <div style={{ margin: 10 }}>
+                <div>
+                    <Input
+                        placeholder={this.state.inputValue}
+                        style={{ width: "250px", marginRight: 10 }}
+                    />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{ margin: "10px", width: "300px" }}>
+                    <List
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={(item) => <List.Item>{item}</List.Item>}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+const defaultState = {
+    inputValue: "Write Something",
+    list: ["喵吉嘻嘻", "凡客体饭卡", "艾跃进中"],
+};
+export default (state = defaultState, action) => {
+    return state;
+};
