360b2f9f2e005c6565e1cbf8620d27d6a5da65d7
digitbase <digitbase@gmail.com>
2020-11-23 21:46:29 +0800

    redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,22 +8,52 @@ class TodoList extends Component {
         super(props);
         console.log(store.getState());
         this.state = store.getState();
+        this.clickBtn = this.clickBtn.bind(this);
+        this.inputChange = this.inputChange.bind(this);
+        this.storeChange = this.storeChange.bind(this);
+        //this.delItem = this.delItem.bind(this);
+        store.subscribe(this.storeChange);
+    }
+    delItem(index) {
+        console.log(index);
+        const action = { type: "delItem", index: index };
+        store.dispatch(action);
+    }
+    clickBtn() {
+        const action = { type: "addItem" };
+        store.dispatch(action);
+    }
+    inputChange(e) {
+        const action = { type: "changeInput", value: e.target.value };
+        store.dispatch(action);
+    }
+
+    storeChange() {
+        this.setState(store.getState());
     }
     render() {
         return (
             <div style={{ margin: 10 }}>
                 <div>
                     <Input
-                        placeholder={this.state.inputValue}
+                        value={this.state.inputValue}
+                        onChange={this.inputChange}
+                        placeholder="write something"
                         style={{ width: "250px", marginRight: 10 }}
                     />
-                    <Button type="primary">增加</Button>
+                    <Button onClick={this.clickBtn} type="primary">
+                        增加
+                    </Button>
                 </div>
                 <div style={{ margin: "10px", width: "300px" }}>
                     <List
                         bordered
                         dataSource={this.state.list}
-                        renderItem={(item) => <List.Item>{item}</List.Item>}
+                        renderItem={(item, index) => (
+                            <List.Item onClick={this.delItem.bind(this, index)}>
+                                {item}
+                            </List.Item>
+                        )}
                     />
                 </div>
             </div>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,28 @@
 const defaultState = {
-    inputValue: "Write Something",
+    inputValue: "",
     list: ["喵吉嘻嘻", "凡客体饭卡", "艾跃进中"],
 };
 export default (state = defaultState, action) => {
+    if (action.type == "changeInput") {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+
+    if (action.type == "addItem") {
+        let newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        //console.log(newState.inputValue);
+        newState.inputValue = "";
+        return newState;
+    }
+
+    if (action.type == "delItem") {
+        let newState = JSON.parse(JSON.stringify(state));
+
+        newState.list.splice(action.value, 1);
+        return newState;
+    }
+
     return state;
 };
